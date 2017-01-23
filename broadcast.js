b3ad456c6d1b45fb65cd97a3fcde57f9f958d0fa
Nick Thomas <nichoth@gmail.com>
2017-01-23 12:04:05 -0800

    use event-emitter-demux for nested events

--- broadcast.js
+++ broadcast.js
@@ -1,36 +1,20 @@
 var h = require('react').createElement
-var Event = require('geval')
 var xtend = require('xtend')
+var Bus = require('event-emitter-demux')
 
 function broadcast (component, events) {
-    var _broadcast = events.reduce(function (acc, ev) {
-        var emit
-        var _ev = Event(function (_emit) {
-            emit = _emit
-        })
-        acc[ev] = {
-            emit: emit,
-            subscribe: _ev
-        }
-        return acc
-    }, {})
+    var bus = Bus(events)
+
     function BroadcastComponent (props) {
         var _props = xtend(props, {
-            emit: Object.keys(_broadcast).reduce(function (acc, k) {
-                acc[k] = _broadcast[k].emit
-                return acc
-            }, {})
+            emit: bus.write
         })
-
         return h(component, _props, [])
     }
 
     return {
         view: BroadcastComponent,
-        on: Object.keys(_broadcast).reduce(function (acc, k) {
-            acc[k] = _broadcast[k].subscribe
-            return acc
-        }, {})
+        on: bus.on
     }
 }
 
